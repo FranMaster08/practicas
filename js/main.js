f9f2a16704cae1fab4d922d2f4af94ea95d7b8e8
Francisco Javier Jimenez Cohen <franjicohen@gmail.com>
2024-10-24 12:53:30 -0300

    add : se grega logica para el popup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
-const main = ()=>{
-    alert('Hola Mundo')
+const elementos = {
+    crearButton: '#crear-btn',
+    popup: '#popup',
+    closePopup: ' #close-popup'
 }
+const AbrirPopUP = () => {
+    const PopUp = document.querySelector(elementos.popup)
+    PopUp.classList.toggle('hidden')
+}
+
+const eventosDeTogglePopUp = () => {
+
+    const elementosArray =[
+        document.querySelector(elementos.crearButton),
+        document.querySelector(elementos.closePopup)
+    ]
+
+    elementosArray.forEach(element => {
+        element.addEventListener('click', () => {
+            AbrirPopUP()
+        })
+    });
+   
+  
+
+
+}
+
+
+
+const CargarEventos = () => {
+    eventosDeTogglePopUp()
+
+}
+
+
+
+
+
+const main = () => {
+    CargarEventos()
+}
+
+
 
 window.onload = main
